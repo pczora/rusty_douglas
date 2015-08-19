eb5aade06334c6821cac23938f31d1490ae537a6
Philipp Czora <pczora@googlemail.com>
2015-08-19 20:27:44 +0200

    Turns out you don't have to explicitly cast literal numbers to usize when using them as indices

--- src/main.rs
+++ src/main.rs
@@ -18,46 +18,39 @@ fn print_vec(v: &Vec<Point>) {
 }
 
 fn douglas_peucker(points: Vec<Point>, eps: f32) -> Vec<Point> {
-    println!("douglas_peucker with points");
-    print_vec(&points);
-    println!("");
     let mut result_list: Vec<Point> = vec![];
     let mut dmax = 0.0;
-    let mut index = 0;
+    let mut index: usize = 0;
     let mut d = 0.0;
-    for i in 1..points.len()-1 {
+    for i in 1..points.len() - 1 {
         d = line_point_distance(points[0], points[points.len() - 1], points[i]);
         if d > dmax {
             dmax = d;
             index = i;
         }
     }
-    println!("dmax: {} at index: {} (point: ({}, {}))", dmax, index, points[index].x, points[index].y);
+
     if dmax > eps {
-        println!("LEFT");
-        let rec_results1 = douglas_peucker(points[0 as usize .. index as usize + 1].to_vec(), eps);
-        println!("RIGHT");
-        let rec_results2 = douglas_peucker(points[index as usize .. (points.len() as usize)].to_vec(), eps);
-        result_list = rec_results1[0 as usize .. rec_results1.len() - 1 as usize].to_vec();
+        let rec_results1 = douglas_peucker(points[0 .. index + 1].to_vec(), eps);
+        let rec_results2 = douglas_peucker(points[index .. (points.len())].to_vec(), eps);
+        result_list = rec_results1[0 .. rec_results1.len() - 1].to_vec();
         result_list.extend(rec_results2.iter().cloned());
     }
     else {
         result_list.push(points[0]);
         result_list.push(points[points.len() - 1]);
     }
-    print_vec(&result_list);
+
     return result_list;
 }
 
 fn main() {
     let l1 = Point {x:0.0, y:0.0};
-    //let l2 = Point {x:5.0, y:0.0};
+    let l2 = Point {x:5.0, y:0.0};
     let l3 = Point {x:10.0, y:5.0};
-    //let l4 = Point {x:20.0, y:1.0};
+    let l4 = Point {x:20.0, y:1.0};
     let l5 = Point {x:25.0, y:0.0};
-    let points: Vec<Point> = vec![l1, l3, l5];
-    //println!("Starting points:");
-    //print_vec(&points);
+    let points: Vec<Point> = vec![l1, l2, l3, l4, l5];
     let result = douglas_peucker(points, 1.0);
     println!("RESULTS");
     print_vec(&result);
